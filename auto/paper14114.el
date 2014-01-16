6245c8bde37612093ac745c0192c485df0d7a946
wobutianl <wobutianl@126.com>
2014-01-16 21:28:51 +0800

    Introduction:
    毕业论文
    Environment:
    Latex
    texLive
    Bug:
    没有封面，前言等。
    Describe:
    1-16 才开始

---
+++ auto/paper14114.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook "paper14114"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "njnuthesis10"
+     "njnuthesis"
+     "master")))
+
---
+++ 南京师范大学论文模板/Clean all useless files.bat
@@ -0,0 +1,17 @@
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.toe /s
+del *.lof 
+del *.lot
+del *.out 
+del *.ps
+del ctextemp_*.tex
+del ctextemp_*.tex.lst
+del ctextemp_main.fen
+del main.fen
